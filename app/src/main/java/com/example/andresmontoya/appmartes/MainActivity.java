11527461af798ca20d76752068281ae7236b6861
andresMontoya <pipemontoya2@hotmail.com>
2017-03-28 20:00:57 -0500

    implementamos el listener del boton

--- app/src/main/java/com/example/andresmontoya/appmartes/MainActivity.java
+++ app/src/main/java/com/example/andresmontoya/appmartes/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.andresmontoya.appmartes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView vista1;
+    Button boton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vista1 = (TextView) findViewById(R.id.view1);
+        boton1 = (Button) findViewById(R.id.button);
+        boton1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        vista1.setText("cambio el texto oprimiendo el boton");
     }
 }
